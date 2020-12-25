bbad9b467d6a613f8309540c021d59297b8cd01a
BalanValeriaCorina <corinaseliste@gmail.com>
2020-12-25 19:28:50 +0200

    creating fragments for authentification process

--- app/src/main/java/com/example/studentmanager/MainActivity.java
+++ app/src/main/java/com/example/studentmanager/MainActivity.java
@@ -1,23 +1,26 @@
 package com.example.studentmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.example.studentmanager.async.AsyncTaskRunner;
 import com.example.studentmanager.async.Callback;
+import com.example.studentmanager.authentication.fragments.RoleChoserFragment;
 import com.example.studentmanager.database.models.Profesor;
 import com.example.studentmanager.database.repositories.ProfesorRepository;
 
 import java.util.List;
 import java.util.concurrent.Callable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     ProfesorRepository profesorRepository;
     private final AsyncTaskRunner taskRunner=new AsyncTaskRunner();
-
+    Fragment fragmentRoleChoser= RoleChoserFragment.newInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 //        Profesor profesor = new Profesor("prof@prof.com", "Prof Test", 1234);
 //        insert(profesor);
 
+        //getSupportFragmentManager().beginTransaction().replace(R.id.placeholder_fragment,fragmentRoleChoser).commit();
 
     }
 
--- app/src/main/java/com/example/studentmanager/authentication/fragments/RoleChoserFragment.java
+++ app/src/main/java/com/example/studentmanager/authentication/fragments/RoleChoserFragment.java
@@ -3,17 +3,20 @@ package com.example.studentmanager.authentication.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.studentmanager.R;
 
 
 public class RoleChoserFragment extends Fragment {
 
-
+    private Button professorbtn;
+    private  Button studentbtn;
 
     public RoleChoserFragment() {
         // Required empty public constructor
@@ -34,6 +37,33 @@ public class RoleChoserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_role_choser, container, false);
+        View view= inflater.inflate(R.layout.fragment_role_choser, container, false);
+        professorbtn=view.findViewById(R.id.profesor_button);
+        studentbtn=view.findViewById(R.id.student_button);
+
+        professorbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle=new Bundle();
+                bundle.putString("role","professor");
+                Navigation.findNavController(view).navigate(R.id.registrationLoginFragment,bundle);
+
+            }
+        });
+
+        studentbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle=new Bundle();
+                bundle.putString("role","student");
+                Navigation.findNavController(view).navigate(R.id.registrationLoginFragment,bundle);
+            }
+        });
+        return view;
     }
+
+
+
+
+
 }
